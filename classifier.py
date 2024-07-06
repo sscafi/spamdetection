8e6979ef96aaeb46fbab64c212f1d14d93a4f748
Saher Scafi <95544378+sscafi@users.noreply.github.com>
2024-07-06 02:29:20 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,40 +1,58 @@
-#spam sms detection
-from sklearn.naive_bayes import *
-from sklearn.dummy import *
-from sklearn.ensemble import *
-from sklearn.neighbors import *
-from sklearn.tree import *
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn.calibration import *
-from sklearn.linear_model import *
-from sklearn.multiclass import *
-from sklearn.svm import *
-import pandas
-
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    AdaBoostClassifier,
+    BaggingClassifier,
+    ExtraTreesClassifier,
+    GradientBoostingClassifier
+)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import (
+    PassiveAggressiveClassifier,
+    RidgeClassifier,
+    RidgeClassifierCV,
+    SGDClassifier,
+    LogisticRegression
+)
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+import pandas as pd
 
 def perform(classifiers, vectorizers, train_data, test_data):
+    """
+    Perform training and evaluation of classifiers with different vectorizers.
+    
+    Parameters:
+    - classifiers: List of classifier instances to evaluate.
+    - vectorizers: List of vectorizer instances to evaluate.
+    - train_data: DataFrame containing training data.
+    - test_data: DataFrame containing test data.
+    """
     for classifier in classifiers:
-      for vectorizer in vectorizers:
-        string = ''
-        string += classifier.__class__.__name__ + ' with ' + vectorizer.__class__.__name__
+        for vectorizer in vectorizers:
+            string = ''
+            string += classifier.__class__.__name__ + ' with ' + vectorizer.__class__.__name__
 
-        # train
-        vectorize_text = vectorizer.fit_transform(train_data.v2)
-        classifier.fit(vectorize_text, train_data.v1)
+            # Train the classifier
+            vectorize_text = vectorizer.fit_transform(train_data.v2)
+            classifier.fit(vectorize_text, train_data.v1)
 
-        # score
-        vectorize_text = vectorizer.transform(test_data.v2)
-        score = classifier.score(vectorize_text, test_data.v1)
-        string += '. Has score: ' + str(score)
-        print(string)
+            # Evaluate performance on test data
+            vectorize_text = vectorizer.transform(test_data.v2)
+            score = classifier.score(vectorize_text, test_data.v1)
+            string += '. Has score: ' + str(score)
+            print(string)
 
-# open data-set and divide it
-data = pandas.read_csv('spam.csv', encoding='latin-1')
-learn = data[:4400] # 4400 items
-test = data[4400:] # 1172 items
+# Load and split the dataset
+data = pd.read_csv('spam.csv', encoding='latin-1')
+learn = data[:4400]  # Training data: first 4400 items
+test = data[4400:]   # Test data: remaining items
 
+# Perform classification with specified classifiers and vectorizers
 perform(
     [
         BernoulliNB(),
